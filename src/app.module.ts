f2c6d64fd2bd9a9d0474b03ed1d229eef426d1cd
Nur <nur.alam@amharctech.com>
2025-10-05 01:10:46 +0600

    production url added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,8 @@ import { JwtModule } from '@nestjs/jwt';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        const uri = configService.get<string>('MONGO_URI');
+        // const uri = configService.get<string>('MONGO_URI');
+        const uri = 'mongodb+srv://furiousnur_db_user:Cb7LmuP13cSA1Cum@cluster0.qhrbfil.mongodb.net/taskdb?retryWrites=true&w=majority';
         if (!uri) throw new Error('Mongo URI not found');
         return { uri };
       },
--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,10 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: 'http://localhost:4000',
+    origin: [
+      'http://localhost:4000',
+      'https://task-management-frontend-mocha-chi.vercel.app'
+    ],
     credentials: true,
   });
 
